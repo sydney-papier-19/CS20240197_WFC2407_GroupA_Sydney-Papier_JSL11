67d8b89671d5da9575d1e494cb9fd6732a0304ba
sydney_p <sydneypapier23@gmail.com>
2024-10-02 00:12:33 +0200

    imported helper functions and initialData to inex.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // TASK: import helper functions from utils
+import { getTasks } from "./utils/taskFunctions";
+import { createNewTask } from "./utils/taskFunctions";
+import { patchTask } from "./utils/taskFunctions";
+import { putTask } from "./utils/taskFunctions";
+import { deleteTask } from "./utils/taskFunctions";
 // TASK: import initialData
+import { initialData } from "./initialData";
 
 
 /*************************************************************************************************************************************************
